517807c7f9e4e9762c583347e275ab7f19a5b7e8
Tutu Daranijo <tutudaranijo@gmail.com>
2023-08-18 09:28:41 -0400

    2 more

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -31,7 +31,7 @@ Cinema table:
 | 1  | War        | great 3D    | 8.9    |
 | 2  | Science    | fiction     | 8.5    |
 | 3  | irish      | boring      | 6.2    |
-| 4  | Ice song   | Fantacy     | 8.6    |
+| 4  | Ice song   | Fantasy     | 8.6    |
 | 5  | House card | Interesting | 9.1    |
 +----+------------+-------------+--------+
 Output: 
@@ -47,5 +47,5 @@ We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 i
 
 SELECT id , movie, description, rating
 FROM Cinema
-WHERE id % 2 <> 0 or description <> "boring" 
+WHERE id % 2 <> 0 or description <> "boring"
 order by rating Desc;
